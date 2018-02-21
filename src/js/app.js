1b2321b02525c3c8698af266c79910097a95bbd9
konradizowski <izowski@gmail.com>
2018-02-21 14:24:40 +0100

    ajax

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,13 @@
 require('ammap3');
 require('../../node_modules/ammap3/ammap/maps/js/worldLow.js');
+require('../css/style.css');
 
 
 document.addEventListener("DOMContentLoaded", function () {
 
     const map = AmCharts.makeChart( "mapdiv", {
-        /**
-         * this tells amCharts it's a map
-         */
         "type": "map",
+
 
         /**
          * create data provider object
@@ -20,7 +19,10 @@ document.addEventListener("DOMContentLoaded", function () {
          */
         "dataProvider": {
             "map": "worldLow",
-            "getAreasFromMap": true
+            "getAreasFromMap": true,
+            "fill": '#A2C5A3'
+
+
         },
 
         /**
@@ -30,18 +32,38 @@ document.addEventListener("DOMContentLoaded", function () {
          */
         "areasSettings": {
             "autoZoom": true,
-            "selectedColor": "#CC0000"
+            "selectedColor": "#CC0033"
         },
 
-        /**
-         * let's say we want a small map to be displayed, so let's create it
-         */
         "smallMap": {}
     } );
 
 
     map.addListener("clickMapObject", function(event) {
-        console.log(event.mapObject.enTitle);
+
+        const selected = event.mapObject.enTitle;
+        console.log(event.mapObject);
+
+        const urlApi = 'https://restcountries.eu/rest/v2/alpha?codes=';
+        function  loadData() {
+            $.ajax({
+                url: urlApi + event.mapObject.id
+            }).done(function (response) {
+                console.log(response);
+
+                document.querySelector('.capital').innerHTML = 'Capital' + response[0].capital;
+                document.querySelector('.currency').innerHTML = 'Currency' + response[0].currencies[0].name;
+                document.querySelector('.languages').innerHTML = 'Languages' + response[0].languages[0].name;
+                document.querySelector('.population').innerHTML = 'Population' + response[0].population;
+
+            }).fail(function (error) {
+                console.log(error);
+            })
+        } loadData();
+        console.log(selected);
+
     });
+
+
 
 });
